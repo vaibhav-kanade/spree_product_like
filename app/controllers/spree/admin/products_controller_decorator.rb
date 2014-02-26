e111f7775d77e308232746703d22136c135345a0
Marek Buszman <mbusiq@gmail.com>
2014-02-26 09:42:54 +0100

    add admin products ranking

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Admin::ProductsController.class_eval do
   def products_ranking
-    @collection = Spree::Product.page(params[:page])
+    @collection = Spree::Product.likes_ranking.page(params[:page])
   end
 end
--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Product.class_eval do
   has_many :likes
+  scope :likes_ranking, ->{ joins(:likes).order('likes_count DESC') }
 
   def liked_by?(user)
     likes.exists?(user: user)
