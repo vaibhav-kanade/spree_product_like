b1c9691d44e5a69b052490f2d78f8310268ff745
Marek Buszman <mbusiq@gmail.com>
2014-02-27 09:16:26 +0100

    like/dislike button moved under product photo, added data-hooks, button look changed

--- app/overrides/products/show.rb
+++ app/overrides/products/show.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(virtual_path: 'spree/products/show',
                      name: 'add_like_to_product',
-                     insert_before: '#product-description[data-hook]',
+                     insert_before: '[data-hook="product_properties"]',
                      partial: 'spree/products/like')
